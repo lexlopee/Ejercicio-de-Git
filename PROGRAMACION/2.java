a6f20c108cc353c5a03c565d76dc89cad466c29e
Alejandro <alexlcabr@gmail.com>
2023-11-16 18:35:10 +0100

    Primer Cambio en la Rama1

---
+++ PROGRAMACION/2.java
@@ -0,0 +1,29 @@
+/*
+ *Ejercicio 11:
+Diseñar un programa que muestre el producto de los 10 primeros números impares.
+ */
+package Ejercicios_Bucles;
+
+/**
+ *
+ * @author alexl
+ */
+public class Ejercicio11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num=1;
+        int producto =1;
+        int contador=0;
+        
+        for(contador=1;contador <10; contador++ ){
+            num =+ 2;
+            producto *=num;
+        }
+            System.out.println(producto);
+            System.out.println("Felicidades conseguiste un numero nuevo")z
+    }
+    
+}
